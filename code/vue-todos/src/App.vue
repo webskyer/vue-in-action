ace17694a7788261051c2da89cc9224d091def0f
webskyer <webskyer@gmail.com>
2017-06-14 17:51:13 +0800

    1.1 插值

---
+++ code/vue-todos/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'vue-todos'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
