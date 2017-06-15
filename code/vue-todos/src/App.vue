22f043731c18f4c6e181a4ba3619d7e09a39fc99
webskyer <webskyer@gmail.com>
2017-06-15 12:13:51 +0800

    显示 todos 数据，使用 Vue 模板的 v-for 标记，形式为 item in items，items是数组，item 是当前数组元素的别名

--- code/vue-todos/src/App.vue
+++ code/vue-todos/src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
-  </div>
+    <ul>
+      <li v-for="todo in todos">
+        <label>{{ todo.value}}</label>
+      </li>
+    </ul>
 </template>
+
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      title: 'vue-todos'
+      title: 'vue-todos',
+      todos: [
+        { value: "阅读一本关于前端开发的书", done: false },
+        { value: "补充示例代码", done: true },
+        { value: "写心得", done: false }
+      ]
     }
   }
 }
 </script>
 
 <style>
+
 </style>
